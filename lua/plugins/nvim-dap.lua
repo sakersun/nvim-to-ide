9d554ffe646792a62e61aa7bfcfc0d19a7a45384
sakersun <sakersun@163.com>
2024-06-28 13:05:14 +0800

    for dap

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -1,3 +1,24 @@
 return {
-	"mfussenegger/nvim-dap",
+	{
+		"mfussenegger/nvim-dap",
+		config = function()
+			require("dap-python").setup("python")
+
+			local dap = require("dap")
+		end,
+	},
+	{
+		"mfussenegger/nvim-dap-python",
+		config = function()
+			local path = "/home/saker/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+			require("dap-python").setup(path)
+		end,
+	},
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+		config = function()
+			require("dapui").setup()
+		end,
+	},
 }
