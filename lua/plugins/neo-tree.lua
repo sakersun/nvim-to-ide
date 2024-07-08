618e5f4fe10091d46c9677ec4c95ff43d21e5861
sakersun <sakersun@163.com>
2024-07-08 16:15:57 +0800

    for neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,4 +6,31 @@ return {
 		"nvim-tree/nvim-web-devicons",
 		"MunifTanjim/nui.nvim",
 	},
+	config = function()
+		vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+		vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+		vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+		vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
+
+		local neotree = require("neo-tree")
+		neotree.setup({
+			filesystem = {
+				filtered_items = {
+					visible = false,
+					hide_dotfiles = false,
+					hide_gitignored = false,
+					hide_hidden = true,
+					hide_by_name = {
+						"__pycache__",
+						".git",
+						".vscode",
+						".venv",
+						".pytest_cache",
+						".nx",
+						"node_modules",
+					},
+				},
+			},
+		})
+	end,
 }
