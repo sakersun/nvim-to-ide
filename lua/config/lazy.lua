af7154c5eb1dab7c4e416fa5ca7acf0aa12ab750
sakersun <sakersun@163.com>
2024-07-08 17:37:46 +0800

    global function

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,3 +20,26 @@ require("lazy").setup({
 		{ import = "plugins" },
 	},
 })
+
+-- 定义自动导入函数
+local function organize_imports()
+	local params = vim.lsp.util.make_range_params()
+	params.context = { only = { "source.organizeImports" } }
+	local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+	for _, res in pairs(result or {}) do
+		for _, r in pairs(res.result or {}) do
+			if r.edit then
+				vim.lsp.util.apply_workspace_edit(r.edit, "utf-16")
+			else
+				vim.lsp.buf.execute_command(r.command)
+			end
+		end
+	end
+end
+
+function MyEcho()
+	print("echo message")
+end
+
+-- 设置快捷键
+vim.api.nvim_set_keymap("n", "<leader>oi", ":lua MyEcho()<CR>", { noremap = true, silent = true })
