14c1a4dafd2f52335963076d463aea41bb3f48d4
sakersun <sakersun@163.com>
2024-07-09 12:09:14 +0800

    for cmp

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -13,7 +13,7 @@ return {
 			cmp.setup({
 				snippet = {
 					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
+						luasnip.lsp_expand(args.body)
 					end,
 				},
 				window = {
