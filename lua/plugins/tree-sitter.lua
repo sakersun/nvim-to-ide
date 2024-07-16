31ec86025444b808c50ac4912161b5d498d4fea0
sakersun <sakersun@163.com>
2024-07-16 13:05:07 +0800

    refactor code

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -5,6 +5,9 @@ return {
 		local configs = require("nvim-treesitter.configs")
 		configs.setup({
 			ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+			auto_install = true,
+			ignore_install = { "go" },
+			modules = {},
 			sync_install = false,
 			highlight = { enable = true },
 			indent = { enable = true },
