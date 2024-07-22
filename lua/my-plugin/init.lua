adac09dca176bff65774914c97af5d22d8a513f5
sakersun <sakersun@163.com>
2024-07-22 14:30:40 +0800

    for my plugin

--- lua/my-plugin/init.lua
+++ lua/my-plugin/init.lua
@@ -4,8 +4,24 @@ function M.my_command()
 	print("this is my-plugin message")
 end
 
+function M.uis()
+	local uis = vim.api.nvim_list_uis()
+	for _, ui in ipairs(uis) do
+		if type(ui) == "table" then
+			for k, v in pairs(ui) do
+				print(tostring(k) .. " : " .. tostring(v))
+			end
+		end
+		print(ui)
+	end
+end
+
 vim.api.nvim_create_user_command("MyCommand", function()
 	M.my_command()
+end, { nargs = 0 })
+
+vim.api.nvim_create_user_command("MyUIS", function()
+	M.uis()
 end, { nargs = 0 })
 
 return M
