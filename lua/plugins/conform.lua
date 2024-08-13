e348b1948549234e382946ed27c675dd16710784
sakersun <sakersun@163.com>
2024-08-13 09:25:12 +0800

    conform lua

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,6 +5,7 @@ return {
 		require("conform").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
+				python = { "isort", "black" },
 			},
 			format_on_save = {
 				timeout_ms = 500,
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -5,6 +5,16 @@ return {
 		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 		lspconfig.lua_ls.setup({
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = { "vim" },
+					},
+					workspace = {
+						library = vim.api.nvim_get_runtime_file("", true),
+					},
+				},
+			},
 			capabilities = capabilities,
 		})
 		lspconfig.tsserver.setup({
