2d76dda267ea70fd12b9948fcf0362b970c97917
sakersun <sakersun@163.com>
2024-08-20 09:22:00 +0800

    enhance healthcheck

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -4,7 +4,10 @@ return {
 	config = function()
 		local configs = require("nvim-treesitter.configs")
 		configs.setup({
-			ensure_installed = { "query", "c", "lua", "vim", "vimdoc" },
+			ensure_installed = { "query", "c", "lua", "vim", "vimdoc", "regex" },
+			ignore_install = {},
+			sync_install = false,
+			modules = {},
 			auto_install = true,
 			highlight = {
 				enabled = true,
