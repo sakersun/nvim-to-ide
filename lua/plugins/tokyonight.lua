502a5e0711ba45d8804eb2a2ef8cce818d373823
sakersun <sakersun@163.com>
2024-10-25 09:21:29 +0800

    update config

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,5 +1,13 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
+	"folke/tokyonight.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("tokyonight").setup({
+			transparent = true,
+			styles = {
+				sidebars = "transparent",
+			},
+		})
+	end,
 }
