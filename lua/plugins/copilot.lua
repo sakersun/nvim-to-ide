3fac564edd45c0d3acba7226eaac44158278f06e
sakersun <sakersun@163.com>
2025-02-18 10:08:13 +0800

    for copilot plugin

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,17 @@
+return {
+	{
+		"zbirenbaum/copilot.lua",
+		config = function()
+			require("copilot").setup({})
+		end,
+	},
+	{
+		"zbirenbaum/copilot-cmp",
+		config = function()
+			require("copilot_cmp").setup()
+		end,
+		dependencies = {
+			"zbirenbaum/copilot.lua",
+		},
+	},
+}
