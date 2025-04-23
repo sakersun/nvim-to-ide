aea19b03f7c2b893b013324bc1760e67f299be15
sakersun <sakersun@163.com>
2025-04-23 16:27:12 +0800

    new plugins

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,5 +1,10 @@
-return { "catppuccin/nvim", name = "catppuccin", priority = 1000, config = function ()
-  require("catppuccin").setup({
-    transparent_background = true
-  })
-end }
+return {
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			transparent_background = true,
+		})
+	end,
+}
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,9 +1,23 @@
 return {
-  {
-    "echasnovski/mini.pairs",
-	  version = "*",
-	  config = function()
-		  require("mini.pairs").setup()
-	  end,
-  }
+	{
+		"echasnovski/mini.pairs",
+		version = "*",
+		config = function()
+			require("mini.pairs").setup()
+		end,
+	},
+	{
+		"echasnovski/mini.indentscope",
+		version = "*",
+		config = function()
+			require("mini.indentscope").setup()
+		end,
+	},
+	{
+		"echasnovski/mini.animate",
+		version = "*",
+		config = function()
+			require("mini.animate").setup()
+		end,
+	},
 }
