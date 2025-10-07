e2a1050ac3edec065bc6e7da3b85f7859636d46b
sakersun <sakersun@163.com>
2025-10-07 21:06:27 +0800

    new plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -32,4 +32,8 @@ require("lazy").setup({
   install = { colorscheme = { "catppuccin" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
+  rocks = {
+    enabled = true,
+    hererocks = true,
+  }
 })
