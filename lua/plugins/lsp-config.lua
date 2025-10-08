11a436010af82b965d11c4e5ce19f39371a8a3bf
sakersun <sakersun@163.com>
2025-10-08 10:42:24 +0800

    new plugins

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,6 +1,15 @@
 return {
   "neovim/nvim-lspconfig",
+  dependencies = { "saghen/blink.cmp" },
   config = function()
-    vim.lsp.enable("lua_ls")
+    local capabilities = require("blink.cmp").get_lsp_capabilities()
+    vim.lsp.config("lua_ls", {
+      capabilities = capabilities,
+      settings = {
+        Lua = {
+          diagnostics = { globals = "vim" },
+        },
+      },
+    })
   end,
 }
