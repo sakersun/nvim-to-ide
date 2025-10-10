b6c7a02fd020186c7146ab3507801f49ba89822c
sakersun <sakersun@163.com>
2025-10-10 23:13:00 +0800

    new config for sign

--- lua/config/config.lua
+++ lua/config/config.lua
@@ -7,3 +7,14 @@ vim.diagnostic.config({
   underline = true,
   severity_sort = true,
 })
+
+vim.diagnostic.config({
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "",
+      [vim.diagnostic.severity.WARN] = "",
+      [vim.diagnostic.severity.INFO] = "",
+      [vim.diagnostic.severity.HINT] = "󰌵",
+    },
+  },
+})
