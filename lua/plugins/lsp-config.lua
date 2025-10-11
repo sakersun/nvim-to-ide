bb3596cca6c528589261098c993009370c2a65b1
sakersun <sakersun@163.com>
2025-10-11 13:51:04 +0800

    refactor code

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -2,26 +2,49 @@ return {
   "neovim/nvim-lspconfig",
   dependencies = { "saghen/blink.cmp" },
   config = function()
+    local on_attach = function(_, bufnr)
+      local map = function(keys, func, desc)
+        vim.keymap.set("n", keys, func, { buffer = bufnr, desc = "LSP: " .. desc, silent = true })
+      end
+
+      local lspbuf = vim.lsp.buf
+
+      map("K", lspbuf.hover, "Hover Documentation")
+      map("<leader>gd", lspbuf.definition, "Go to Definition")
+      map("<leader>gr", lspbuf.references, "Go to References")
+      map("<leader>ca", lspbuf.code_action, "Code Action")
+    end
+
+    local lspconfig = vim.lsp
     local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-    vim.lsp.config("lua_ls", {
-      capabilities = capabilities,
-      settings = {
-        Lua = {
-          runtime = { version = "LuaJIT" },
-          diagnostics = { globals = { "vim" } },
-          workspace = {
-            checkThirdParty = false,
-            library = {
-              vim.env.VIMRUNTIME,
-              "${3rd}/luv/library",
-              "${3rd}/busted/library",
+    local servers = {
+      lua_ls = {
+        settings = {
+          Lua = {
+            runtime = { version = "LuaJIT" },
+            diagnostics = { globals = { "vim" } },
+            workspace = {
+              checkThirdParty = false,
+              library = {
+                vim.env.VIMRUNTIME,
+                "${3rd}/luv/library",
+                "${3rd}/busted/library",
+              },
             },
           },
         },
       },
-    })
-    vim.lsp.enable("lua_ls")
-    vim.lsp.enable("pyright")
+      pyright = {
+        settings = {},
+      },
+    }
+
+    for name, opts in pairs(servers) do
+      opts =
+        vim.tbl_deep_extend("force", { on_attach = on_attach, capabilities = capabilities }, opts)
+      lspconfig.config(name, opts)
+      lspconfig.enable(name)
+    end
   end,
 }
