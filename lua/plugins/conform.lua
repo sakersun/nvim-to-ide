c582b4549e71914ebee964f838ba632e902972ec
sakersun <sakersun@163.com>
2026-01-08 17:08:29 +0800

    add toml formatter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -15,6 +15,7 @@ return {
         html = { "prettier" },
         json = { "prettier" },
         markdown = { "prettier" },
+        toml = { "taplo" },
       },
       formatters = {
         swift_format = {
