3d1da9bee1464f5fe0db96aad10637264ad8b8fe
sakersun <sakersun@163.com>
2026-01-13 16:52:32 +0800

    for ts autotag

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,4 +3,12 @@ return {
   branch = "master",
   lazy = false,
   build = ":TSUpdate",
+  opts = {
+    highlight = {
+      enable = true,
+    },
+    indent = {
+      enable = true,
+    },
+  },
 }
